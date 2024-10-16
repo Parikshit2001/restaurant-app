bfbe6e634b1f153b54bfb6a9b9fafb6f87c27bca
Parikshit <parikshitgupta2001@gmail.com>
2024-10-16 19:44:34 +0530

    Add cors middleware

--- app/api/owner/getmenu/route.ts
+++ app/api/owner/getmenu/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/app/db";
 import { NextResponse } from "next/server";
 
+export const revalidate = 10;
+
 export async function GET() {
   const dishes = await prisma.dish.findMany({});
   console.log(dishes);
